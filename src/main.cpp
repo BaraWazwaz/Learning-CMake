363c0f57ad95b11b9d151edfca5590c4d2c4df94
BaraWazwaz <wazwazbaraa@gmail.com>
2026-08-25 13:59:45 +0300

    added fmt

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,9 @@
 #include <iostream>
+#include <fmt/core.h>
 #include "header.hpp"
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    fmt::print("Hello {}! Automated CMake setups are great!\n", "Docker-ready world");
     std::cout << get_secret() << std::endl;
     return 0;
 }
