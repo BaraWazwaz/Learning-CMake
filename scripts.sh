049273bf360d1efb2b2bc3624046d80c27278d16
BaraWazwaz <wazwazbaraa@gmail.com>
2026-08-25 11:55:37 +0300

    forgot that I was inside  when I used

---
+++ scripts.sh
@@ -0,0 +1,27 @@
+#!/bin/env bash
+
+# 1. CMake Set up
+step_001() {
+    nano ./CMakeLists.txt
+    # write its content
+}
+
+# 2. Build Set up
+step_002() {
+    mkdir ./build
+    cd ./build
+    cmake ..
+}
+
+# 3. Build Projects' Executables
+step_003() {
+    cmake --build .
+}
+
+# 4. Run Executables
+step_004() {
+    ./CMakeHelloWorld.exe
+    ./SecondProject.exe
+}
+
+echo "Open this file to view the scripts, this script file does not run anything by itself"
